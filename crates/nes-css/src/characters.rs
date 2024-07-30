54f368557b84fc7be28c124ca48d31b6331e3607
Son <leson.phung@gmail.com>
2024-07-30 10:28:40 +1000

    Make module docs for nes css characters

--- crates/nes-css/src/characters.rs
+++ crates/nes-css/src/characters.rs
@@ -1,4 +1,5 @@
-/// Nintendo owns the copyright of these characters. Please comply with the Nintendo guidelines and laws of the applicable jurisdiction.
+///! Nintendo owns the copyright of these characters. Please comply with the Nintendo guidelines and laws of the applicable jurisdiction.
+
 pub use pokeball::*;
 
 mod pokeball;
